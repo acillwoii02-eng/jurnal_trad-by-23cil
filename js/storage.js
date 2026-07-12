a7910295642bfd02fe70e9ffc022f0ace70d168a
acillwoii02-eng <acillwoii02@gmail.com>
2026-07-12 18:55:43 +0700

    Add functions to show and close trade form

--- js/storage.js
+++ js/storage.js
@@ -14,3 +14,29 @@ function addTrade(trade) {
     trades.push(trade);
     saveTrades(trades);
 }
+
+function showForm(){
+document.getElementById("tradeForm").style.display="block";
+}
+
+function closeForm(){
+document.getElementById("tradeForm").style.display="none";
+}
+
+function saveTrade(){
+
+const pair=document.getElementById("pair").value;
+
+const type=document.getElementById("type").value;
+
+const entry=Number(document.getElementById("entry").value);
+
+const exit=Number(document.getElementById("exit").value);
+
+addTrade(pair,type,entry,exit);
+
+closeForm();
+
+location.reload();
+
+}
